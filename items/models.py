60aaafb4f9f643d1def15bed564049a8a9dd872c
Balkaneros91 <>
2023-06-25 23:28:29 +0000

    Add testimonials

--- items/models.py
+++ items/models.py
@@ -49,3 +49,25 @@ class Product(models.Model):
             self.rating = self.generate_random_rating()
 
         super().save(*args, **kwargs)
+
+
+# class Sculpture(models.Model):
+#     id = models.UUIDField(
+#         default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+#     title = models.CharField(max_length=200, unique=True)
+#     description = models.CharField(max_length=200)
+#     detailed_description = models.TextField(max_length=500)
+#     price = models.DecimalField(max_digits=6, decimal_places=2)
+#     image = CloudinaryField('image', default='placeholder')
+#     available = models.BooleanField(default=False)
+#     quantity = models.DecimalField(max_digits=3, decimal_places=0, default=0)
+#     created_on = models.DateTimeField(auto_now_add=True)
+
+#     class Meta:
+#         """
+#         Helper function to organize sculptures by newest first
+#         """
+#         ordering = ['-created_on']
+
+#     def __str__(self):
+#         return str(self.title)
--- testimonials/admin.py
+++ testimonials/admin.py
@@ -1,1 +1,9 @@
 from django.contrib import admin
+from .models import Review
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_filter = ('name', 'email', 'created_on', 'public')
+    list_display = ('name', 'email', 'created_on', 'public')
+    search_fields = ('name', 'email', 'created_on', 'public')
