a228692e9bd0a85172c24afa6d1e547e0ea1d4a6
Balkaneros91 <>
2023-06-27 10:20:27 +0000

    Add

--- items/urls.py
+++ items/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('', views.all_products, name='items'),
     path('<int:pk>/', views.product_detail, name='items_detailed'),
     path('add/', views.add_product, name='add_item'),
-    # path('edit/<int:product_id>/', views.edit_product, name='edit_itemt'),
-    # path('delete/<int:product_id>/', views.delete_product, name='delete_item'),  # noqa
+    path('edit/<int:product_id>/', views.edit_product, name='edit_item'),
+    path('delete/<int:product_id>/', views.delete_product, name='delete_item'),  # noqa
 ]
--- static/script.js
+++ static/script.js
@@ -5,14 +5,6 @@ setTimeout(function () {
 }, 5000);
 
 
-// document.addEventListener('DOMContentLoaded', function () {
-//     setTimeout(function () {
-//         var messageElement = document.getElementById('msg');
-//         if (messageElement) {
-//             messageElement.classList.add('hide');
-//         }
-//     }, 5000);
-// });
 
 $(document).ready(function () {
     $('.dropdown-toggle').dropdown();
